feaa1cb56eb72b7389a734d442c57baeebdaf4db
Chocohead <Chocohead@Aroma1997.org>
2021-07-18 02:49:00 +0100

    Load the extension sooner
    Extensions are selected when the Mixin transformer's environment changes, which is before Early Risers run

--- src/com/chocohead/cc/Extension.java
+++ src/com/chocohead/cc/Extension.java
@@ -1,17 +1,23 @@
 package com.chocohead.cc;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
 import org.objectweb.asm.tree.ClassNode;
 
 import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 import org.spongepowered.asm.mixin.transformer.IMixinTransformer;
 import org.spongepowered.asm.mixin.transformer.ext.Extensions;
 import org.spongepowered.asm.mixin.transformer.ext.IExtension;
 import org.spongepowered.asm.mixin.transformer.ext.IExtensionRegistry;
 import org.spongepowered.asm.mixin.transformer.ext.ITargetClassContext;
 
-public class EarlyRiser implements Runnable, IExtension {
+public class Extension implements IMixinConfigPlugin, IExtension {
 	@Override
-	public void run() {
+	public void onLoad(String mixinPackage) {
 		Object transformer = MixinEnvironment.getCurrentEnvironment().getActiveTransformer();
 		if (!(transformer instanceof IMixinTransformer)) throw new IllegalStateException("Running with an odd transformer: " + transformer);
 
@@ -26,8 +32,37 @@ public class EarlyRiser implements Runnable, IExtension {
 		return true;
 	}
 
+
+	@Override
+	public List<String> getMixins() {
+		return Collections.emptyList();
+	}
+
+	@Override
+	public String getRefMapperConfig() {
+		return null;
+	}
+
+	@Override
+	public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+	}
+
+	@Override
+	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+		return true;
+	}
+
+
 	@Override
 	public void preApply(ITargetClassContext context) {
+	}
+
+	@Override
+	public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+	}
+
+	@Override
+	public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
 	}
 
 	@Override
